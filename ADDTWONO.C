ded0890b0130cb6c5e1cbf38a474fd346fe05a54
NikhilVijay97 <nikhilvijayan2004@gmail.com>
2025-09-15 11:18:04 +0530

    Add files via upload

---
+++ ADDTWONO.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,sum=0;
+ clrscr();
+ printf("\n enter two numbers:");
+ scanf("%d%d",&a,&b);
+ sum=a+b;
+ printf("sum is %d",sum);
+ getch();
+ }
